8ae17ff95760809f80d776c5b966f9a5f2d01b72
ilmars <9987548+darksworm@users.noreply.github.com>
2019-10-01 21:21:51 +0300

    WM hints for floating & fullscreen window #23

--- src/gui/WindowManager.cpp
+++ src/gui/WindowManager.cpp
@@ -1,4 +1,5 @@
 #include "WindowManager.h"
+#include <X11/Xatom.h>
 
 typedef struct {
     unsigned long flags;
@@ -16,31 +17,60 @@ unsigned long RGBA2DWORD(int iR, int iG, int iB, int iA)
 void WindowManager::setWindowSettings() {
     XClassHint *class_hints = XAllocClassHint();
 
-    class_hints->res_name = "hksel";
-    class_hints->res_class = "hksel";
+    class_hints->res_name = "imgsel";
+    class_hints->res_class = "imgsel";
 
     XStoreName(display, this->window, class_hints->res_name);
     XSetClassHint(display, this->window, class_hints);
     XFree(class_hints);
 
-    Atom wm_delete_win = XInternAtom(display, "WM_DELETE_this->windowDOW", 0);
+    Atom wm_delete_win = XInternAtom(display, "WM_TAKE_FOCUS", 1);
     XSetWMProtocols(display, this->window, &wm_delete_win, 1);
-
-    XMapWindow(display, this->window);
 
     Hints hints;
     Atom property;
 
-    hints.flags = 2; // changing this->window decorations
-    hints.decorations = 0; // disabling this->window decorations
+    hints.flags = 2; // changing window decorations
+    hints.decorations = 0; // disabling window decorations
 
     property = XInternAtom(display, "_MOTIF_WM_HINTS", true);
-
     XChangeProperty(display, this->window, property, property, 32, PropModeReplace, (unsigned char *) &hints, 5);
 
-    XMapRaised(display, this->window);
+    auto window_type = XInternAtom(display, "_NET_WM_WINDOW_TYPE", false);
+    property = XInternAtom(display, "_NET_WM_WINDOW_TYPE_SPLASH", false);
+    XChangeProperty(display, this->window, window_type, XA_ATOM, 32, PropModeReplace, reinterpret_cast<unsigned char*>(&property), 1);
 
+    auto windowState = XInternAtom(display, "_NET_WM_STATE", false);
+    Atom windowStates[] = {
+        XInternAtom(display, "_NET_WM_STATE_MODAL", false),
+        XInternAtom(display, "_NET_WM_STATE_ABOVE", false),
+        XInternAtom(display, "_NET_WM_STATE_STAYS_ON_TOP", false)
+    };
+
+    XChangeProperty(display, this->window, windowState, XA_ATOM, 32, PropModeReplace, reinterpret_cast<unsigned char*>(&windowStates), 1);
+
+    property = XInternAtom(display, "I3_FLOATING_WINDOW", false);
+    XChangeProperty(display, this->window, property, XA_CARDINAL, 32, PropertyNewValue, reinterpret_cast<unsigned char*>(&property), 1);
+
+	XSizeHints *size_hints = XAllocSizeHints();
+	size_hints->min_width = this->getScreen()->width;
+	size_hints->min_height = this->getScreen()->height;
+	size_hints->max_width = this->getScreen()->width;
+	size_hints->max_height = this->getScreen()->height;
+	size_hints->width = this->getScreen()->width;
+	size_hints->height = this->getScreen()->height;
+	size_hints->x = 0;
+	size_hints->y = 0;
+	size_hints->win_gravity = CenterGravity;
+	size_hints->flags = PMinSize|PMaxSize|PPosition|PSize|PWinGravity;
+	XSetWMNormalHints(display, window, size_hints);
+
+	XFree(size_hints);
+
+    XMapWindow(display, this->window);
+    XMapRaised(display, this->window);
     XSelectInput(display, this->window, StructureNotifyMask);
+    XFlush(display);
 }
 
 Display* WindowManager::getDisplay() {
@@ -76,8 +106,7 @@ void WindowManager::newWindow() {
     // TODO : this should be 0 to be transparent
     attr.background_pixel = RGBA2DWORD(0, 0, 0, 200);
 
-    // when setting y to 0, in i3 the window appears in a negative y position, 1 works tho
-    this->window = XCreateWindow(display, DefaultRootWindow(display), 0, 1, (unsigned) width, (unsigned) height - 1, 0,
+    this->window = XCreateWindow(display, DefaultRootWindow(display), 0, 0, (unsigned) width, (unsigned) height, 0,
                                vInfo.depth, InputOutput,
                                vInfo.visual, CWColormap | CWBorderPixel | CWBackPixel | FocusChangeMask, &attr);
 
