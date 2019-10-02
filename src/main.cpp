5f264e2a8ab14b066c60450966ba4df30b9c271b
ilmars <9987548+darksworm@users.noreply.github.com>
2019-10-02 20:48:24 +0300

    #15 - half assed multi-threading.

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@
 #include "input/handler/instruction/FilterInstruction.h"
 #include "config/ConfigManager.h"
 #include "input/handler/instruction/CopyInstruction.h"
+#include "util/ThreadSafeQueue.h"
 
 #include <glob.h> // glob(), globfree()
 #include <vector>
@@ -92,91 +93,75 @@ void drawText(WindowManager *windowManager, const std::string &text, Dimensions 
     XFreeGC(display, gc);
 }
 
-int main(int argc, char *argv[]) {
-    // TODO: is /tmp a good directory for a pid file?
-    int pid_file = open("/tmp/imgsel.pid", O_CREAT | O_RDWR, 0666);
-    int rc = flock(pid_file, LOCK_EX | LOCK_NB);
-    if (rc && EWOULDBLOCK == errno) {
-        std::cout << "another proccess is already running!";
-        exit(1);
-    }
+struct XEventWrapper {
+    int eventType;
+    KeySym keySym;
+};
 
-    std::vector<std::string> imageFiles = glob(argv[1]);
-    auto config = std::unique_ptr<Config>(ConfigManager::getOrLoadConfig());
-
-    std::vector<std::string> imageExtensions = {
-            "jpg",
-            "jpeg",
-            "png",
-            "gif"
-    };
-
-    std::vector<Image> images;
-    for (const auto &img:imageFiles) {
-        for (const auto &ext:imageExtensions) {
-            if (0 == img.compare(img.length() - ext.length(), ext.length(), ext)) {
-                images.emplace_back(img);
-                break;
-            }
-        }
-    }
-
-    std::unique_ptr<WindowManager> windowManager(new WindowManager());
-    std::unique_ptr<ImagePickerDrawer> itemPickerDrawer(
-            new ImagePickerDrawer(windowManager.get(), &images)
-    );
-
-    Display *display = windowManager->getDisplay();
-    Window window = windowManager->getWindow();
-
-    XKeyboardState initKBState;
-    XGetKeyboardControl(display, &initKBState);
-
-    InputMode state = InputMode::TEXT_FILTER;
-    int keep_running = 1;
-    XEvent event;
+void handleXInput(WindowManager *windowManager, ThreadSafeQueue<XEventWrapper> *eventQueue, bool *shouldQuit) {
+    auto display = windowManager->getDisplay();
+    auto window = windowManager->getWindow();
 
     XSelectInput(display, window, ExposureMask | KeyPressMask | KeyReleaseMask | ButtonPressMask | StructureNotifyMask);
-    itemPickerDrawer->drawFrame(&*images.begin());
+    XEvent event;
 
-    std::unique_ptr<InputHandler> inputHandler(nullptr);
+    bool keepRunning = true;
 
-    unsigned eventType = 0;
-
-    while (keep_running) {
-        unsigned keyCode = 0;
-
-        // read xevents
+    while (keepRunning) {
         while (XPending(display)) {
             XNextEvent(display, &event);
 
             switch (event.type) {
                 case DestroyNotify:
                 case UnmapNotify:
-                    keep_running = 0;
+                    eventQueue->push(XEventWrapper{
+                            .eventType = -1
+                    });
+                    keepRunning = false;
                     break;
 
+                case KeyRelease:
                 case KeyPress: {
-                    eventType = KeyPress;
-                    keyCode = x11_keycode_to_libinput_code(XLookupKeysym(&event.xkey, 0));
+                    eventQueue->push(XEventWrapper{
+                            .eventType = event.type,
+                            .keySym = XLookupKeysym(&event.xkey, 0)
+                    });
                     break;
                 }
 
-                case KeyRelease:
-                    eventType = KeyRelease;
-                    keyCode = x11_keycode_to_libinput_code(XLookupKeysym(&event.xkey, 0));
-                    break;
-
-                case ConfigureNotify:
-                    XClearWindow(windowManager->getDisplay(), windowManager->getWindow());
-                    auto hk = itemPickerDrawer->getSelectedImage() ? itemPickerDrawer->getSelectedImage()
-                                                                   : &*images.begin();
-                    itemPickerDrawer->drawFrame(hk);
-                    drawText(windowManager.get(), "You're in TEXT_FILTER mode", Dimensions(900, 150));
-                    drawText(windowManager.get(), "To change mode, press the CAPS LOCK key", Dimensions(900, 160));
+                default:
+                    eventQueue->push(XEventWrapper{
+                            .eventType = event.type
+                    });
                     break;
             }
         }
+        std::this_thread::sleep_for(std::chrono::nanoseconds(16600000));
+
+        if (*shouldQuit) {
+            keepRunning = false;
+        }
+    }
+}
+
+void handleEvents(WindowManager *windowManager, ThreadSafeQueue<XEventWrapper> *eventQueue,
+                  std::vector<Image> images, bool *shouldQuit) {
+    std::unique_ptr<ImagePickerDrawer> itemPickerDrawer(
+            new ImagePickerDrawer(windowManager, &images)
+    );
+
+    itemPickerDrawer->drawFrame(&*images.begin());
+
+    std::unique_ptr<InputHandler> inputHandler(nullptr);
+
+    bool keep_running = true, readEvent;
+    XEventWrapper event;
+    InputMode state = InputMode::TEXT_FILTER;
+
+    while (keep_running) {
+        unsigned keyCode = 0;
+
+        readEvent = eventQueue->next(event);
 
         /*
         * TODO: this probably shouldn't be a static value
@@ -185,7 +170,22 @@ int main(int argc, char *argv[]) {
         */
         std::this_thread::sleep_for(std::chrono::nanoseconds(16600000));
 
-        if (keyCode == 0) {
+        if (!readEvent) {
+            continue;
+        }
+
+        if (event.eventType == -1) {
+            keep_running = false;
+            continue;
+        }
+
+        if (event.eventType == ConfigureNotify) {
+            XClearWindow(windowManager->getDisplay(), windowManager->getWindow());
+            auto hk = itemPickerDrawer->getSelectedImage() ? itemPickerDrawer->getSelectedImage()
+                                                           : &*images.begin();
+            itemPickerDrawer->drawFrame(hk);
+            drawText(windowManager, "You're in TEXT_FILTER mode", Dimensions(900, 150));
+            drawText(windowManager, "To change mode, press the CAPS LOCK key", Dimensions(900, 160));
             continue;
         }
 
@@ -193,25 +193,30 @@ int main(int argc, char *argv[]) {
             inputHandler.reset(InputHandlerFactory::getInputHandler(state));
         }
 
+        if (event.eventType == KeyPress || event.eventType == KeyRelease) {
+            keyCode = x11_keycode_to_libinput_code(event.keySym);
+        }
+
         printf("RAW: %s FORMATTED: %s %u\n", keycode_linux_rawname(keyCode),
                keycode_linux_name(keycode_linux_to_hid(keyCode)), keyCode);
 
         std::unique_ptr<Instruction> instruction;
 
-        if (eventType == KeyPress) {
+        if (event.eventType == KeyPress) {
             instruction = std::unique_ptr<Instruction>(inputHandler->handleKeyPress(keyCode));
-        } else {
+        } else if (event.eventType == KeyRelease) {
             instruction = std::unique_ptr<Instruction>(inputHandler->handleKeyRelease(keyCode));
-        }
+        } else continue;
 
         if (instruction->getType() == InstructionType::NONE) {
             continue;
         }
 
-        bool shouldExit = itemPickerDrawer->getFilterString().empty() && instruction->getType() == InstructionType::CANCEL;
+        bool shouldExit =
+                itemPickerDrawer->getFilterString().empty() && instruction->getType() == InstructionType::CANCEL;
 
         if (shouldExit || instruction->getType() == InstructionType::EXIT) {
-            keep_running = 0;
+            keep_running = false;
             continue;
         }
 
@@ -260,19 +265,68 @@ int main(int argc, char *argv[]) {
             // TODO: handle 's in filenames
             std::string command = "cat '" + path + "' | xclip -selection clipboard -target image/" + ext + " -i";
             system(command.c_str());
-            keep_running = 0;
+            keep_running = false;
         }
 
-        if (config->isIsDebug()) {
-            static const std::string inputModes[] = {"SELECTION", "TEXT_FILTER"};
-            drawText(windowManager.get(), "You're in " + inputModes[(int) state] + " mode", Dimensions(900, 150));
-            drawText(windowManager.get(), "To change mode, press the CAPS LOCK key", Dimensions(900, 160));
+        static const std::string inputModes[] = {"SELECTION", "TEXT_FILTER"};
+        drawText(windowManager, "You're in " + inputModes[(int) state] + " mode", Dimensions(900, 150));
+        drawText(windowManager, "To change mode, press the CAPS LOCK key", Dimensions(900, 160));
 
-            if (!itemPickerDrawer->getFilterString().empty()) {
-                drawText(windowManager.get(), "QUERY: " + itemPickerDrawer->getFilterString(), Dimensions(500, 100));
+        if (!itemPickerDrawer->getFilterString().empty()) {
+            drawText(windowManager, "QUERY: " + itemPickerDrawer->getFilterString(), Dimensions(500, 100));
+        }
+    }
+
+    *shouldQuit = true;
+}
+
+int main(int argc, char *argv[]) {
+    // TODO: is /tmp a good directory for a pid file?
+    int pid_file = open("/tmp/imgsel.pid", O_CREAT | O_RDWR, 0666);
+    int rc = flock(pid_file, LOCK_EX | LOCK_NB);
+    if (rc && EWOULDBLOCK == errno) {
+        std::cout << "another proccess is already running!";
+        exit(1);
+    }
+
+    std::vector<std::string> imageFiles = glob(argv[1]);
+    auto config = std::unique_ptr<Config>(ConfigManager::getOrLoadConfig());
+
+    std::vector<std::string> imageExtensions = {
+            "jpg",
+            "jpeg",
+            "png",
+            "gif"
+    };
+
+    std::vector<Image> images;
+    for (const auto &img:imageFiles) {
+        for (const auto &ext:imageExtensions) {
+            if (0 == img.compare(img.length() - ext.length(), ext.length(), ext)) {
+                images.emplace_back(img);
+                break;
             }
         }
     }
+
+    auto windowManager = new WindowManager();
+
+    Display *display = windowManager->getDisplay();
+    windowManager->getWindow();
+
+    XKeyboardState initKBState;
+    XGetKeyboardControl(display, &initKBState);
+
+    ThreadSafeQueue<XEventWrapper> eventQueue;
+    bool shouldQuit;
+
+    XInitThreads();
+
+    std::thread xEventListener(handleXInput, windowManager, &eventQueue, &shouldQuit);
+    std::thread xEventHandler(handleEvents, windowManager, &eventQueue, images, &shouldQuit);
+
+    xEventListener.join();
+    xEventHandler.join();
 
     XKeyboardState exitKBState;
     XGetKeyboardControl(display, &exitKBState);
@@ -290,6 +344,6 @@ int main(int argc, char *argv[]) {
     }
 
     windowManager->destroyWindow();
-
+    delete windowManager;
     return 0;
 }
